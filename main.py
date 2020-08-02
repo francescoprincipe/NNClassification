2b318d731cf45b741e883dc77663ef003ccff69b
francescoprincipe <fra97.p@gmail.com>
2020-08-02 18:15:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+class_names = ['apple', 'banana', 'plum', 'cherry', 'grape',
+               'tomato', 'potato', 'pear', 'peach', 'pepper']
