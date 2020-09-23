6ce291ec3f9e3c12ff053d1aaa5e348eb5de3c20
francescoprincipe <fra97.p@gmail.com>
2020-09-23 18:54:53 +0200

    pre release version

--- baseClassification.py
+++ baseClassification.py
@@ -91,7 +91,7 @@ print(test_labels)
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(32, 32, 3)),
-    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
@@ -99,7 +99,16 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=30)
+#model.fit(train_images, train_labels, epochs=30)
+history = model.fit(train_images, train_labels, epochs=30,
+                    validation_data=(test_images, test_labels))
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+#plt.show()
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
--- cnnClassification.py
+++ cnnClassification.py
@@ -60,13 +60,13 @@ test_images = test_images / 255.0
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+#model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+#model.add(layers.MaxPooling2D((2, 2)))
+#model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+#model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(10))
 
 model.compile(optimizer='adam',
@@ -82,7 +82,7 @@ plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-
+plt.show()
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
 
